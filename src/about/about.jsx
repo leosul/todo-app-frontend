c79f19088c257a055faa1809e765e4d1763581f6
Leonardo <leosul@gmail.com>
2019-11-24 21:05:39 +0000

    Create basics Components and routes

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='About' small='me'></PageHeader>
+
+        <h2>Leonardo Jacques da Silva</h2>
+        <p>Nationality: Brazil</p>
+        <p>Live in: Ireland</p>
+        <p>Telephone: +353 0834223515</p>
+        <p>E-mail: leosul@icloud.com</p>
     </div>
 )
