56586c8e3bc11fc3b8b45d1110d959532cc6fc0b
Leonardo <leosul@gmail.com>
2019-11-26 21:52:15 +0000

    Add basic events in react conponents

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -4,10 +4,11 @@ import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='todoForm'>
-
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Add one task'></input>
+                placeholder='Add a task'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
